ebcc75dad7ecef85ab1898d72b3752f01bb3c1e1
Yulia <mamaeva.jula@gmail.com>
2022-10-12 15:17:04 +0300

    HW2

---
+++ 2/Task_2.py
@@ -0,0 +1,8 @@
+# Задача 2. Выведите таблицу истинности для выражения ¬(X ∧ Y) ∨ Z.
+
+print('x y z')
+for x in range(2):
+  for y in range(2):
+    for z in range(2):
+      if (not(x) and y and z) or (not(x) and not(y) and z) or (not(x) and not (y) and not(z))==1:
+        print(x, y, z)
